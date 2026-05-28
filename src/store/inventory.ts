ab62e8e411e4756b561e7a27365aef90e4d945d5
Holly Phillips <hollymphillips.dev@gmail.com>
2026-05-28 01:28:52 -0700

    no more env for the api base. we're goin official

--- src/store/inventory.ts
+++ src/store/inventory.ts
@@ -1,7 +1,7 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { LocationObj, ContainerObj, ItemObj, DigitsCountObj, CategoriesObj, ItemSubmission, FullDataObj } from '@/constants/db-interface';
 
-const API_BASE = process.env.EXPO_PUBLIC_API_BASE as string;
+const API_BASE = "https://hollyngrade.com/rfid";
 
 export const inventoryKeys = {
     all: ['inventory'] as const,
